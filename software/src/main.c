ced3090a5b6cec0ab4bd744d834cebf3e9f35234
rmingon <ronanmingon69550@gmail.com>
2025-12-27 17:41:46 +0100

    fix led pin

--- software/src/main.c
+++ software/src/main.c
@@ -12,28 +12,34 @@ extern void vPortSVCHandler(void);
 extern void xPortPendSVHandler(void);
 extern void xPortSysTickHandler(void);
 
-static void LedTask(void *argument) {
+static void LedTask(void *argument)
+{
 	(void)argument;
-	while (1) {
-		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+	while (1)
+	{
+		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_8);
 		vTaskDelay(pdMS_TO_TICKS(250));
 	}
 }
 
-void SVC_Handler(void) {
+void SVC_Handler(void)
+{
 	vPortSVCHandler();
 }
 
-void PendSV_Handler(void) {
+void PendSV_Handler(void)
+{
 	xPortPendSVHandler();
 }
 
-void SysTick_Handler(void) {
+void SysTick_Handler(void)
+{
 	HAL_IncTick();
 	xPortSysTickHandler();
 }
 
-int main(void) {
+int main(void)
+{
 	HAL_Init();
 	SystemClock_Config();
 	MX_GPIO_Init();
@@ -44,11 +50,13 @@ int main(void) {
 	xTaskCreate(LedTask, "led", 128, NULL, tskIDLE_PRIORITY + 1, NULL);
 	vTaskStartScheduler();
 
-	while (1) {
+	while (1)
+	{
 	}
 }
 
-static void SystemClock_Config(void) {
+static void SystemClock_Config(void)
+{
 	RCC_OscInitTypeDef oscillator_config = {0};
 	RCC_ClkInitTypeDef clock_config = {0};
 
@@ -59,8 +67,10 @@ static void SystemClock_Config(void) {
 	oscillator_config.PLL.PLLState = RCC_PLL_ON;
 	oscillator_config.PLL.PLLSource = RCC_PLLSOURCE_HSE;
 	oscillator_config.PLL.PLLMUL = RCC_PLL_MUL9;
-	if (HAL_RCC_OscConfig(&oscillator_config) != HAL_OK) {
-		while (1) {
+	if (HAL_RCC_OscConfig(&oscillator_config) != HAL_OK)
+	{
+		while (1)
+		{
 		}
 	}
 
@@ -71,36 +81,42 @@ static void SystemClock_Config(void) {
 	clock_config.APB1CLKDivider = RCC_HCLK_DIV2;
 	clock_config.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&clock_config, FLASH_LATENCY_2) != HAL_OK) {
-		while (1) {
+	if (HAL_RCC_ClockConfig(&clock_config, FLASH_LATENCY_2) != HAL_OK)
+	{
+		while (1)
+		{
 		}
 	}
 }
 
-static void MX_GPIO_Init(void) {
-	__HAL_RCC_GPIOC_CLK_ENABLE();
+static void MX_GPIO_Init(void)
+{
+	__HAL_RCC_GPIOA_CLK_ENABLE();
 
 	GPIO_InitTypeDef gpio = {0};
-	gpio.Pin = GPIO_PIN_13;
+	gpio.Pin = GPIO_PIN_8;
 	gpio.Mode = GPIO_MODE_OUTPUT_PP;
 	gpio.Pull = GPIO_NOPULL;
 	gpio.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOC, &gpio);
+	HAL_GPIO_Init(GPIOA, &gpio);
 
-	/* BluePill PC13 LED is typically active-low. Start off. */
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
 }
 
-void vApplicationMallocFailedHook(void) {
+void vApplicationMallocFailedHook(void)
+{
 	taskDISABLE_INTERRUPTS();
-	while (1) {
+	while (1)
+	{
 	}
 }
 
-void vApplicationStackOverflowHook(TaskHandle_t task, char *task_name) {
+void vApplicationStackOverflowHook(TaskHandle_t task, char *task_name)
+{
 	(void)task;
 	(void)task_name;
 	taskDISABLE_INTERRUPTS();
-	while (1) {
+	while (1)
+	{
 	}
 }
